1dff651744c0d7b689c5a866aff60a3536710aef
bodogedaisuki <111432099+bodogedaisuki@users.noreply.github.com>
2022-08-17 20:58:09 +0900

    Update main.js

--- main.js
+++ main.js
@@ -20,3 +20,24 @@ phina.main(function() {
   });
   app.run();
 });
+
+phina.define('MainScene', {
+  superClass: 'DisplayScene',
+  init: function() { // ここはコンストラクタです。要は一番最初に一回だけやる処理のこと。
+    this.superInit(); // 親クラス（DisplayScene）のコンストラクタを呼んでます。
+
+　　　// 以下追記です
+    // newをする必要はありません
+    // 直接、必要なオブジェクトの種類を書くと、そのオブジェクトが作成されます。
+    // 文字：Label()、キャラ：Sprite()、円形：CircleShape()とかとか
+    // 最後の.addChildTo(this)は、作成されたLabelオブジェクトをMainSceneの子供にするよってことです。
+    // まず画面があって、そこに必要なオブジェクトを置く、という流れです。
+    var label = Label('オッス、, おらphina.js!').addChildTo(this)
+    // これ以降でlabelに対して様々な操作をする
+    // 「.（ドット）」でつなげていくことで、オブジェクトに対し色々できます。
+    // .addChildToで画面に置いたり、setPositionで置く座標を決めたり
+    // Javascriptは最後に「；（セミコロン）」を置くことで、行の終わりの意味になります。
+    // つまり上の「var label =」から始まって、下の「gridY.center());」までが１行なんです。
+      .setPosition(this.gridX.center(), this.gridY.center()); // 位置を指定
+  }
+});
